1ec2752aedfeb8d2fc2bd2480393bf8f3b56cddc
Aaditya3636 <aaditya.bodke@i2econsulting.com>
2026-05-05 09:51:50 +0530

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 
 st.title("Clinical Trial Dashboard")
 
-uploaded_file = st.file_uploader("Upload a CSV file (optional)", type=["csv"])
+uploaded_file = None
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
